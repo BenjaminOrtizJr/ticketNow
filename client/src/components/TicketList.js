e761a9660abd134d0c0915904fa63ba9f7c56f09
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2022-03-07 17:20:51 -0500

    #

--- client/src/components/TicketList.js
+++ client/src/components/TicketList.js
@@ -1,17 +1,12 @@
 import React, {useContext} from 'react'
 import Ticket from '../components/Ticket'
-import { UserContext } from '../context/UserProvider'
 
 const TicketList = (props) => {
   const { tickets } = props
 
-  const {
-    deleteTicket
-  } = useContext(UserContext)
-
   return (
     <div className="ticket-list">
-      {tickets.map(ticket => <Ticket {...ticket} key={ticket._id} deleteTicket={deleteTicket}/>)}
+      {tickets.map(ticket => <Ticket {...ticket} key={ticket._id} />)}
     </div>
   )
 }
