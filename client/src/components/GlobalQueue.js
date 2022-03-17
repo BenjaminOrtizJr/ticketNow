5cd507e77fc17c651974f483f19b46ae44bbbc9a
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2022-03-17 11:38:47 -0400

    #

--- client/src/components/GlobalQueue.js
+++ client/src/components/GlobalQueue.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import GlobalList from '../components/GlobalList'
 import { UserContext } from '../context/UserProvider'
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined'
 
 const GlobalQueue = (props) => {
 // test
@@ -16,6 +17,18 @@ const GlobalQueue = (props) => {
     return (
         <div className="ticket-queue">
             <h1>Global Queue</h1>
+            <div className="queue-headers">
+                <span className="span11"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Number</p></span>
+                <span className="span22"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Caller</p></span>
+                <span className="span33"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Location</p></span>
+                <span className="span44"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Priority</p></span>
+                <span className="span55"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Call type</p></span>
+                <span className="span66"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Contact type</p></span>
+                <span className="span77"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Opened by</p></span>
+                <span className="span88"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Opened</p></span>
+                <span className="span99"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Short description</p></span>
+                <span className="span100"><MenuOutlinedIcon style={{ fontSize: 16 }}/><p className="queue-header-text">Description</p></span>
+            </div>
             <GlobalList tickets={tickets} />
         </div>
     )
--- client/src/components/TicketQueue.js
+++ client/src/components/TicketQueue.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import TicketList from '../components/TicketList'
 import { UserContext } from '../context/UserProvider'
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined'
 
 const TicketQueue = (props) => {
 // test
@@ -16,6 +17,18 @@ const TicketQueue = (props) => {
     return (
         <div className="ticket-queue">
             <h1>My Queue</h1>
+            <div className="queue-headers">
+                <span className="span1"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Number</p></span>
+                <span className="span2"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Caller</p></span>
+                <span className="span3"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Location</p></span>
+                <span className="span4"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Priority</p></span>
+                <span className="span5"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Call type</p></span>
+                <span className="span6"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Contact type</p></span>
+                <span className="span7"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Opened by</p></span>
+                <span className="span8"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Opened</p></span>
+                <span className="span9"><MenuOutlinedIcon style={{ fontSize: 16 }} /><p className="queue-header-text">Short description</p></span>
+                <span className="span10"><MenuOutlinedIcon style={{ fontSize: 16 }}/><p className="queue-header-text">Description</p></span>
+            </div>
             <TicketList tickets={tickets} />
         </div>
     )
